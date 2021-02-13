4b8b99a4a61e00de1ba4f32c6c208c8c88eb7549
Adam Chaplin <contact@adamchaplin.uk>
2021-02-13 21:23:42 +0000

    Added support for internet explorer and fixed mobile bug

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	// Check for smartphones and tablets (Doesn't detect touch screen PCs)
+	var touchDevice = 'ontouchstart' in document.documentElement;
 	// Smooth scrolling for links
 	$("a").on('click', function(event) {
 
@@ -74,15 +76,24 @@ $(document).ready(function() {
 	
 	// Function that updates positions of all headings and lines
 	function scrollEffect() {
+		let ratio = screen.height / screen.width * 15
 		// Loop through each heading item
 		Array.prototype.forEach.call(headingItems, function(element) {
 			// Magic maths that updates the left margin depending on the position of it's background_layer
-			element.style.marginLeft =  Math.min(-(element.parentElement.getBoundingClientRect().top/6) + 50, 50) + "vw"
+			element.style.marginLeft =  Math.min(-(element.parentElement.getBoundingClientRect().top/ratio) + 50, 50) + "vw"
 		});
 		// Loop through each heading line item
-		Array.prototype.forEach.call(headingLineItems, function(element) {
-			// Magic maths that updates the left margin depending on the position of it's background_layer
-			element.style.marginLeft =  Math.max((element.parentElement.getBoundingClientRect().top/6) + 50, 50) + "vw"
-		});
+		if(touchDevice) {
+			// Mobile devices break when underline is too far right so it comes in with the heading from the left
+			Array.prototype.forEach.call(headingLineItems, function(element) {
+				// Magic maths that updates the left margin depending on the position of it's background_layer
+				element.style.marginLeft =  Math.min(-(element.parentElement.getBoundingClientRect().top/ratio) + 50, 50) + "vw"
+			});
+		} else {
+			Array.prototype.forEach.call(headingLineItems, function(element) {
+				// Magic maths that updates the left margin depending on the position of it's background_layer
+				element.style.marginLeft =  Math.max((element.parentElement.getBoundingClientRect().top/ratio) + 50, 50) + "vw"
+			});
+		}
 	}
 });
